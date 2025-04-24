f04d82ed022dcee8b88221cc94e54a129a5add8a
José Vitor Soares <josevitorgoncalvessoares@gmail.com>
2025-04-24 12:25:46 -0300

    ✨ feat: add entities

---
+++ src/domain/entities/game.entity.ts
@@ -0,0 +1,10 @@
+export type GameEntity = {
+  id: number;
+  date: string;
+  homeTeam: string;
+  homeTeamScore: number;
+  homeAbbreviation: string;
+  visitorTeam: string;
+  visitorTeamScore: number;
+  visitorAbbreviation: string;
+};
